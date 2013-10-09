6b88c66d1773d1783a7214a7e2f66cacda5e0383
a20251313 <a20251313@163.com>
2013-10-09 13:27:19 +0800

    add resource

---
+++ readApp/JFSetModel.h
@@ -0,0 +1,16 @@
+//
+//  JFSetModel.h
+//  readApp
+//
+//  Created by ran on 13-10-9.
+//  Copyright (c) 2013年 com.jingfu.ran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFSetModel : NSObject<NSCoding>
+{
+    
+}
+
+@end
---
+++ readApp/JFSetModel.m
@@ -0,0 +1,28 @@
+//
+//  JFSetModel.m
+//  readApp
+//
+//  Created by ran on 13-10-9.
+//  Copyright (c) 2013年 com.jingfu.ran. All rights reserved.
+//
+
+#import "JFSetModel.h"
+
+@implementation JFSetModel
+
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    
+}
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self)
+    {
+    }
+    
+    return self;
+}
+
+@end
